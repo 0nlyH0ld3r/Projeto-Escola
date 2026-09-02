20c16ee71c7092f78ac7d80648ab9f4e81037447
0nlyH0ld3r <manin.d2020@gmail.com>
2026-09-01 21:51:39 -0300

    Unificando as structs Professor e Aluno em uma struct única  Individuo,além de ter adicionado também o atributo 'eh_doscente' à struct para diferenciar doscentes e discentes

--- cod1.c
+++ cod1.c
@@ -2,53 +2,60 @@
 #include <termios.h>
 #include <unistd.h>
 
-typedef struct {
-	unsigned int	matricula;
-	unsigned int	nascimento;
-	unsigned int	cpf;
-	char		nome[40];
-	char		genero; // F | M
-} Aluno;
+#define true	1
+#define false	0
 
-typedef struct {
-	unsigned int	matricula;
-	unsigned int	nascimento;
-	unsigned int	cpf;
-	char		nome[40];
-	char		genero; // F | M
-} Professor;
+// Declaração de disciplinas.
+typedef struct Individuo Individuo; 
+typedef struct Disciplina Disciplina;
 
-typedef struct {
-	unsigned int	nascimento;
-	Professor*	professor;
-	Aluno*		lista_alunos;
-	char		nome[40];
-	char		codigo[6];
-} Disciplina;
-
-int vaga_vazia (void* lista, size_t t, char tipo);
-int cadastro_aluno (Aluno* lista, int index);
-int cadastro_professor (Professor* lista, size_t t);
+void input_string(char *string, size_t t);
+int input_char();
+int procura_vaga (void* lista, size_t t, char tipo);
+int cadastro (Individuo* lista, int index);
 int cadastro_disciplina (Disciplina* lista, size_t t);
 
+struct Individuo{
+	char*		nome;		// Nota: Não tentem escrever nada por cima disso.
+	Disciplina*	disciplinas;
+	unsigned int	matricula;
+	unsigned int	nascimento;
+	unsigned int	cpf;
+	unsigned int	n_disciplinas;	// Número de disciplinas do individuo;
+	char		genero;		// F | M
+	char	eh_doscente;
+	
+};
 
+struct Disciplina {
+	char*		nome;
+	char*		codigo;
+	Individuo*	professor;
+	Individuo*	lista_alunos;
+	unsigned int	nascimento;
+};
 
-int vaga_vazia (void* lista, size_t t, char tipo) {
-	if (tipo == 'A') {
-		Aluno* p = (Aluno*)lista;
+int input_char() {
+	struct termios old_t;
+	tcgetattr(STDIN_FILENO, &old_t);
+	struct termios new_t;
+	new_t.c_lflag &= ~(ICANON | ECHO);
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &new_t );
+
+	int ch = getchar();
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &old_t );
+
+	return ch;
+}
+
+int procura_vaga (void* lista, size_t t, char tipo) {
+	if (tipo == 'P') {
+		Individuo* p = (Individuo*)lista;
 
 		for (int i = 0; i < t; ++i) {
 			if (p[i].matricula == 0) {
-				return i;
-			}
-		}
-	}
-
-	else if (tipo == 'P') {
-		Professor* p = (Professor*)lista;
-		
-		for (int i = 0; i < t; ++i) {
-			if (p[i].matricula == 0) { 
 				return i;
 			}
 		}
@@ -67,15 +74,16 @@ int vaga_vazia (void* lista, size_t t, char tipo) {
 	return -1;
 }
 
-int cadastro_aluno (Aluno* lista, int index) {
+int cadastro_aluno (Individuo* lista, int index) {
 	if (index == -1) return 1;
 
-	Aluno* x = &lista[index];
+	Individuo* x = &lista[index];
+	x->nome = "Bruno"; 
 
 
 	return 0;
 }
 
-int cadastro_professor (Professor* lista, size_t t);
+int cadastro_professor (Individuo* lista, size_t t);
 
 int cadastro_disciplina (Disciplina* lista, size_t t);
